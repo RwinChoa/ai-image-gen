dfdcb252472a970440ed217cb089d1eff4755840
Baojun Zhao <baojunzhao@Baojuns-MacBook-Air.local>
2022-12-10 11:18:15 +1300

    api finished

--- api/index.js
+++ api/index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/openai', require('./routes/openaiRoutes'))
+app.use('/api/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
